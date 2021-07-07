6e7782425ea8a7fd9dc4dfb2ef2e857708fea917
Catherine Lydia <p.cath.reens@gmail.com>
2021-07-07 16:57:42 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+const fb_credentials = require('./facebook_credentials');
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId:fb_credentials.clientId_FB,
+        clientSecret: fb_credentials.clientSecret_FB
+    }
 }
